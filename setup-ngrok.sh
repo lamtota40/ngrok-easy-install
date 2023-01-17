19c2aaaaf51f8ad632c21bc763fab8156ca6a426
lamtota40 <lamtota40@gmail.com>
2023-01-17 12:32:30 +0700

    Update setup-ngrok.sh

--- setup-ngrok.sh
+++ setup-ngrok.sh
@@ -31,6 +31,7 @@ echo "Wait 20s…"
 sleep 20
 echo -e "Finish… to check status NGROK: \n http://127.0.01:4040"
 STATUSNGROK=$(curl -s http://127.0.0.1:4040/api/tunnels | jq '.tunnels | .[] | "\(.name) \(.public_url)"')
+echo -e "To stop srrvice NGROK:\n ./ngrok service stop"
 echo -e "To setting configuration:\n ngrok.yml"
 echo -e "To change authtoken:\n ./ngrok config add-authtoken 2J8ncba…"
 echo -e "service online:\n" $STATUSNGROK
