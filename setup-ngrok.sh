c9ccba7b34a4ec53077e2a814ccf5a4b8f3caee1
lamtota40 <lamtota40@gmail.com>
2023-01-28 16:37:42 +0700

    Update setup-ngrok.sh

--- setup-ngrok.sh
+++ setup-ngrok.sh
@@ -19,7 +19,7 @@ ARCHIVE=ngrok-v3-stable-linux-$ARCH.zip
 DOWNLOAD_URL=https://bin.equinox.io/c/bNyj1mQVY4c/$ARCHIVE
 
 sudo apt-get update
-sudo apt-get install wget unzip jq -y
+sudo apt-get install unzip jq -y
 wget https://raw.githubusercontent.com/lamtota40/ngrok-easy-install/main/ngrok.yml
 wget $DOWNLOAD_URL
 unzip $ARCHIVE
