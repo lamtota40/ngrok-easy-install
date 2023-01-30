d5c18ab753128e6cbfa7bd5ee456722d66dfc2bc
lamtota40 <lamtota40@gmail.com>
2023-01-30 15:42:20 +0700

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,5 +1,62 @@
 #!/usr/bin/env bash
 
+if (( $EUID != 0 )); then
+    echo "Please run as root"
+    echo "You can Try comand 'su root' or 'sudo -i'"
+    exit 1
+fi
+
+lock1=/var/lib/dpkg/lock-frontend
+lock2=/var/lib/dpkg/lock
+lock3=/var/cache/apt/archives/lock
+
+if [ -f "$lock1" ];then
+if [ -z $(lsof -t $lock1) ]
+then
+      echo "Ok... file ($lock1) already delete"
+else
+      sudo kill -9 $(lsof -t $lock1)
+      echo "Found..PID ($lock1) already kill & delete file"
+fi
+sudo rm -rf $lock1
+fi
+##############################
+if [ -f "$lock2" ];then
+if [ -z $(lsof -t $lock2) ]
+then
+      echo "Ok... file ($lock2) already delete"
+else
+      sudo kill -9 $(lsof -t $lock2)
+      echo "Found..PID ($lock2) already kill & delete file"
+fi
+sudo rm -rf $lock2
+fi
+##############################
+if [ -f "$lock3" ];then
+if [ -z $(lsof -t $lock3) ]
+then
+      echo "Ok... file ($lock3) already delete"
+else
+      sudo kill -9 $(lsof -t $lock3)
+      echo "Found..PID ($lock3) already kill & delete file"
+fi
+sudo rm -rf $lock3
+fi
+
+sudo apt-get install --reinstall libappstream4
+sudo dpkg --configure -a
+sudo apt-get update
+
+sudo apt-get install openssh-server -y
+
+if [ ! $(which wget) ]; then
+    sudo apt-get install wget -y
+fi
+
+if [ ! $(which jq) ]; then
+    sudo apt-get install jq -y
+fi
+#for download manual https://dl.equinox.io/ngrok/ngrok-v3/stable/archive
 # determine system arch
 ARCH=
 if [ "$(uname -m)" == 'x86_64' ]
@@ -15,31 +72,40 @@ else
     ARCH=arm
 fi
 
-ARCHIVE=ngrok-v3-stable-linux-$ARCH.zip
+ARCHIVE=ngrok-v3-stable-linux-$ARCH.tgz
 DOWNLOAD_URL=https://bin.equinox.io/c/bNyj1mQVY4c/$ARCHIVE
 
-sudo apt-get update
 sudo apt-get install jq -y
-sudo apt-get install unzip jq -y
+mkdir -p /opt/ngrok
+cd /opt/ngrok
 wget https://raw.githubusercontent.com/lamtota40/ngrok-easy-install/main/ngrok.yml
+sudo wget https://raw.githubusercontent.com/lamtota40/ngrok-easy-install/main/ngrok.service -P /lib/systemd/system/
+#cp ngrok.service /lib/systemd/system/
 wget $DOWNLOAD_URL
-unzip $ARCHIVE
+tar xvf $ARCHIVE
+rm $ARCHIVE
 sudo chmod +x ngrok
 clear
-echo "Running ngrok for $ARCH . . ."
-./ngrok service install --config=ngrok.yml
-sleep 3
-./ngrok service start
-echo "Wait 15s…"
-sleep 15
+echo "Running ngrok for ARCH $(uname -m) . . ."
+#./ngrok service install --config=ngrok.yml
+systemctl enable ngrok.service
+systemctl start ngrok.service
+#./ngrok service start
+echo "Wait 10s…"
+sleep 10
 echo -e "Finish… to check status NGROK: \n http://127.0.01:4040"
-echo -e "To stop service NGROK:\n ./ngrok service stop"
-echo -e "To setting configuration:\n ngrok.yml"
+echo "First enter comand 'cd /opt/ngrok'"
+echo -e "To stop service NGROK:\n systemctl stop ngrok.service"
+echo -e "To setting configuration:\n sudo nano ngrok.yml"
 echo -e "To change authtoken:\n ./ngrok config add-authtoken 2J8ncba…"
-#if [ ! $(which jq) ]; then
-#    echo 'Please install git package'
-#    exit 1
-#fi
 
-STATUSNGROK=$(curl -s http://127.0.0.1:4040/api/tunnels | jq '.tunnels | .[] | "\(.name) \(.public_url)"')
-echo -e "service online NGROK:\n" $STATUSNGROK
+if [ ! $(which jq) ]; then
+    echo -e "service online NGROK:\n"
+    wget http://127.0.0.1:4040/api/tunnels -q -O -
+else
+    STATUSNGROK=$(wget http://127.0.0.1:4040/api/tunnels -q -O - | jq '.tunnels | .[] | "\(.name) \(.public_url)"')
+    echo -e "service online NGROK:\n" $STATUSNGROK
+fi
+cd
+
+#End script
