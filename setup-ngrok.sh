007653d8eb2ec14bae3b59176732002f6e79678d
lamtota40 <lamtota40@gmail.com>
2023-01-30 13:22:39 +0700

    Update setup-ngrok.sh

--- setup-ngrok.sh
+++ setup-ngrok.sh
@@ -3,6 +3,7 @@
 if (( $EUID != 0 )); then
     echo "Please run as root"
     echo "You can Try comand 'su - root' or 'sudo -i'"
+    echo "After you change to root Enter comand 'bash setup-ngrok.sh' "
     exit 1
 fi
 
