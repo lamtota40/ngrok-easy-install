d4266e6dfbab487f9ef14ec61890e396cbd8045d
lamtota40 <lamtota40@gmail.com>
2023-01-30 08:38:29 +0700

    Update setup-ngrok.sh

--- setup-ngrok.sh
+++ setup-ngrok.sh
@@ -80,6 +80,7 @@ sleep 3
 echo "Wait 15s…"
 sleep 15
 echo -e "Finish… to check status NGROK: \n http://127.0.01:4040"
+echo "First enter comand 'cd /opt/ngrok'"
 echo -e "To stop service NGROK:\n ./ngrok service stop"
 echo -e "To setting configuration:\n ngrok.yml"
 echo -e "To change authtoken:\n ./ngrok config add-authtoken 2J8ncba…"
