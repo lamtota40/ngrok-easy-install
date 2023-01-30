38ff5b674adf45c473fbf0f0ee23c58e0de6f87d
lamtota40 <lamtota40@gmail.com>
2023-01-30 15:20:44 +0700

    Update setup-ngrok.sh

--- setup-ngrok.sh
+++ setup-ngrok.sh
@@ -13,6 +13,49 @@ if [ ! $(which jq) ]; then
     sudo apt-get install jq -y
 fi
 
+lock1=/var/lib/dpkg/lock-frontend
+lock2=/var/lib/dpkg/lock
+lock3=/var/cache/apt/archives/lock
+
+if [ -f "$lock1" ];then
+if [ -z $(lsof -t $lock1) ]
+then
+      echo "Ok... file ($lock1) already delete"
+else
+      sudo kill -9 $(lsof -t $lock1)
+      echo "Found..PID ($lock1) already kill & delete file"
+fi
+sudo rm -rf $lock1
+fi
+##############################
+if [ -f "$lock2" ];then
+if [ -z $(lsof -t $lock2) ]
+then
+      echo "Ok... file ($lock2) already delete"
+else
+      sudo kill -9 $(lsof -t $lock2)
+      echo "Found..PID ($lock2) already kill & delete file"
+fi
+sudo rm -rf $lock2
+fi
+##############################
+if [ -f "$lock3" ];then
+if [ -z $(lsof -t $lock3) ]
+then
+      echo "Ok... file ($lock3) already delete"
+else
+      sudo kill -9 $(lsof -t $lock3)
+      echo "Found..PID ($lock3) already kill & delete file"
+fi
+sudo rm -rf $lock3
+fi
+
+sudo apt-get install --reinstall libappstream4
+sudo dpkg --configure -a
+sudo apt-get update
+
+sudo apt-get install openssh-server -y
+
 #for download manual https://dl.equinox.io/ngrok/ngrok-v3/stable/archive
 # determine system arch
 ARCH=
