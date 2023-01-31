5cf6ad28e55790efe1a3beb891ea7388d8edc257
lamtota40 <lamtota40@gmail.com>
2023-01-31 15:02:34 +0700

    Update setup-ngrok.sh

--- setup-ngrok.sh
+++ setup-ngrok.sh
@@ -5,6 +5,7 @@ if (( $EUID != 0 )); then
     echo "You can Try comand 'su root' or 'sudo -i' or 'sudo -'"
     exit 1
 fi
+sudo apt-get update
 
 if [ ! $(which wget) ]; then
     sudo apt-get install wget -y
