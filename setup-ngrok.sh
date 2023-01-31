1a38769f044fc1c9a95d49429fd8dced28e64379
lamtota40 <lamtota40@gmail.com>
2023-01-31 14:58:26 +0700

    Update setup-ngrok.sh

--- setup-ngrok.sh
+++ setup-ngrok.sh
@@ -2,40 +2,9 @@
 
 if (( $EUID != 0 )); then
     echo "Please run as root"
-    echo "You can Try comand 'su root' or 'sudo -i'"
+    echo "You can Try comand 'su root' or 'sudo -i' or 'sudo -'"
     exit 1
 fi
-
-sudo pkill -9 apt.systemd
-echo "wait 10s"
-sleep 10
-sudo pkill -9 apt.systemd
-lock1=/var/lib/apt/lists/lock
-lock2=/var/lib/dpkg/lock-frontend
-lock3=/var/lib/dpkg/lock
-lock4=/var/cache/apt/archives/lock
-
-for (( x=1; x<=4; x++ ))
-do 
- plock="lock$x"
- if [ -f "${!plock}" ];then
-     if [ -z $(lsof -t ${!plock}) ]
-     then
-        echo "Ok... file (${!plock}) already delete"
-     else
-        sudo kill -9 $(lsof -t ${!plock})
-        echo "Found..PID (${!plock}) already kill & delete file"
-     fi
- sudo rm ${!plock}
- fi
-done
-
-sudo dpkg --configure -a
-sudo apt-get install --reinstall libappstream4 -y
-sudo apt-get install -f
-sudo apt-get update
-
-sudo apt-get install openssh-server -y
 
 if [ ! $(which wget) ]; then
     sudo apt-get install wget -y
